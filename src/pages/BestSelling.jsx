44c7c454631e1900ce8fb23aa924bd7a27b07c44
MohdNadeem <codewithnadeem@gmail.com>
2023-10-15 22:37:20 +0530

    remove bestselling page

--- src/pages/BestSelling.jsx
+++ src/pages/BestSelling.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 
 const BestSelling = () => {
-  return (
-    <div className="bg-blue-500 p-4 rounded-lg shadow-md">
-      <h2 className="text-2xl font-bold text-white mb-4">
-        Best Selling Products
-      </h2>
-      <ul className="list-disc list-inside text-white">
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
-        {/* Add more products as needed */}
-      </ul>
-    </div>
-  );
+  return <div>BestSelling</div>;
 };
 
 export default BestSelling;
